07e40e0373e16023c6c3d65c09ead257ab7904b6
marco-marcox <96086997+marco-marcox@users.noreply.github.com>
2026-08-27 11:57:58 +0200

    Add health API

---
+++ api/health.js
@@ -0,0 +1,1 @@
+import{configured,GMI_MODEL,send}from'../lib/gmi.js';export default async function handler(req,res){if(req.method!=='GET')return send(res,405,{error:'Method not allowed'});return send(res,200,{ok:true,configured:configured(),model:GMI_MODEL})}
